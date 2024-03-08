3d81be2cfb35baf86984c64534270b0e76dfb31f
sagarsinghkk <135123825+sagarsinghkk@users.noreply.github.com>
2024-03-08 14:26:45 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -6,33 +6,7 @@ using namespace std;
 vector<string> hold;
 bool error = false;
 int pc = 0;
-unordered_map<string, string> mp = {
-    {"zero", "x0"},
-    {"ra", "x1"},
-    {"sp", "x2"},
-    {"gp", "x3"},
-    {"tp", "x4"},
-    {"t0", "x5"},
-    {"t1", "x6"},
-    {"t2", "x7"},
-    {"s0", "x8"},
-    {"s1", "x9"},
-    {"a0", "x10"},
-    {"a1", "x11"},
-    {"a2", "x12"},
-    {"a3", "x13"},
-    {"a4", "x14"},
-    {"a5", "x15"},
-    {"a6", "x16"},
-    {"a7", "x17"},
-    {"s2", "x18"},
-    {"s3", "x19"},
-    {"s4", "x20"},
-    {"s5", "x21"},
-    {"s6", "x22"},
-    {"s7", "x23"},
-    {"s8", "x24"},
-};
+
 void splitString(string str)
 {
   string inputString = str;
@@ -388,12 +362,12 @@ int main()
     else if (hold[4 * i] == "auipc" || hold[4 * i] == "lui")
     {
 
-      ans = U_format(hold[4 * i], hold[4 * i + 1], hold[4 * i + 2], hold[4 * i + 3]);
+      ans = U_format(hold[4 * i], hold[4 * i + 1], hold[4 * i + 2]);
     }
     else if (hold[4 * i] == "jal")
     {
 
-      ans = UJ_format(hold[4 * i], hold[4 * i + 1], hold[4 * i + 2], hold[4 * i + 3]);
+      ans = UJ_format(hold[4 * i], hold[4 * i + 1], hold[4 * i + 2]);
     }
 
     write(ans);
